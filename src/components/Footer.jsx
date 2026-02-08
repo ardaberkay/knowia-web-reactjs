184b935f1ad1601b5e3d0de1e17192c5ae1c471b
Arda Berkay Bağım <ardabeb@hotmail.com>
2026-02-09 02:14:32 +0300

    -

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,26 +1,30 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
+import { Link } from "react-router-dom";
 
 export default function Footer() {
   const { t } = useTranslation();
   return (
-<footer className="w-full bg-transparent px-4 bottom-0 mt-auto">
-  <div className="max-w-7xl mx-auto border-t border-gray-400 px-4 flex flex-col md:flex-row items-center py-4 justify-between gap-4 md:gap-0 text-center md:text-left">
-    <div>
-      <p className="text-sm">Powered by BB Software</p>
-    </div>
-    <div>
-      <p className="text-sm">&copy; {new Date().getFullYear()} {t("footer.rightReserved")}</p>
-    </div>
-    <div className="flex flex-col sm:flex-row gap-2 md:gap-4">
-      <a href="/policy" className="text-sm hover:text-gray-200">
-        {t("footer.privacyPolicy")}
-      </a>
-      <a href="/terms" className="text-sm hover:text-gray-200">
-        {t("footer.termsOfService")}
-      </a>
-    </div>
-  </div>
-</footer>
+    <footer className="w-full bg-transparent px-4 bottom-0 mt-auto">
+      <div className="max-w-7xl mx-auto border-t border-gray-400 px-4 flex flex-col md:flex-row items-center py-4 justify-between gap-4 md:gap-0 text-center md:text-left">
+        <div>
+          <p className="text-sm">Powered by BB Software</p>
+        </div>
+        <div>
+          <p className="text-sm">
+            &copy; {new Date().getFullYear()} {t("footer.rightReserved")}
+          </p>
+        </div>
+        <div className="flex flex-col sm:flex-row gap-2 md:gap-4">
+          <Link to="/policy" className="text-sm hover:text-gray-200">
+            {t("footer.privacyPolicy")}
+          </Link>
+
+          <Link to="/terms" className="text-sm hover:text-gray-200">
+            {t("footer.termsOfService")}
+          </Link>
+        </div>
+      </div>
+    </footer>
   );
 }
